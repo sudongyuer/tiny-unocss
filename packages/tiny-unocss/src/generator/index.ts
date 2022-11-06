bf7597b20f416ebf117d9b83c309752435db764c
Frozen FIsh <976499226@qq.com>
2022-11-06 23:53:18 +0800

    chore: add generator

--- packages/tiny-unocss/src/generator/index.ts
+++ packages/tiny-unocss/src/generator/index.ts
@@ -1,8 +1,38 @@
 import type { TinyUnocssRule } from '../types'
 
 export function generator(rules: TinyUnocssRule[]) {
+  const cache = new Map<string, string | null>()
   return (code: string) => {
     const tokens = new Set(code.split(/[\s'"`;]/g))
     const css: string[] = []
+
+    tokens.forEach((token) => {
+      if (cache.has(token)) {
+        const r = cache.get(token)
+        if (r)
+          css.push(r)
+        tokens.delete(token)
+      }
+    })
+
+    for (const [matcher, handler] of rules) {
+      tokens.forEach((token) => {
+        const match = token.match(matcher)
+        if (match) {
+          const result = handler(...Array.from(match))
+          if (result) {
+            css.push(result)
+            cache.set(token, result)
+            tokens.delete(token)
+          }
+        }
+      })
+    }
+
+    tokens.forEach((token) => {
+      cache.set(token, null)
+    })
+
+    return css.join('\n')
   }
 }
