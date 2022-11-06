203076e0faa31d746efb5bbeaa643b045c3e1695
Frozen FIsh <976499226@qq.com>
2022-11-07 00:14:18 +0800

    chore: update generator test

--- packages/tiny-unocss/test/generator.test.ts
+++ packages/tiny-unocss/test/generator.test.ts
@@ -1,4 +1,7 @@
 import { describe, expect, it } from 'vitest'
+import { cssEscape } from '../src/uitls/cssEscape'
+import type { TinyUnocssRule } from '../src/types'
+import { generator } from '../src/generator'
 
 describe('generator', () => {
   it('tokens', () => {
@@ -13,6 +16,28 @@ describe('generator', () => {
         "",
         "hello",
       }
+    `)
+  })
+
+  it('generator', () => {
+    const directionMap: Record<string, string> = {
+      'l': '-left',
+      'r': '-right',
+      't': '-top',
+      'b': '-bottom',
+      '': '',
+    }
+    const rule: TinyUnocssRule
+      = [/^p([trbl]?)-(\d)$/, (f, d, s) => {
+        return `.${cssEscape(f)} { padding${directionMap[d] || ''} : ${(+s) / 4}rem; }`
+      }]
+    const code = 'pl-4 pt-2 pr-8 pb-12'
+    const generate = generator([rule])
+    const generateCss = generate(code)
+    expect(generateCss).toMatchInlineSnapshot(`
+      ".pl-4 { padding-left : 1rem; }
+      .pt-2 { padding-top : 0.5rem; }
+      .pr-8 { padding-right : 2rem; }"
     `)
   })
 })
