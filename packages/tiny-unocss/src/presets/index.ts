62518f5c8e1bed17f69809910c2a843190cd743b
Frozen FIsh <976499226@qq.com>
2022-11-08 23:01:39 +0800

    chore: update presets

--- packages/tiny-unocss/src/presets/index.ts
+++ packages/tiny-unocss/src/presets/index.ts
@@ -3,5 +3,5 @@ import { directionMap } from '../maps'
 import type { TinyUnocssRule } from '../types'
 
 export const defaultRules: TinyUnocssRule[] = [
-  [/^p([trbl]?)-(\d)$/, (f, d, s) => { return `.${cssEscape(f)} { padding${directionMap[d] || ''} : ${(+s) / 4}rem; }` }],
+  [/^p([trbl]?)-(\d+)$/, (f, d, s) => { return `.${cssEscape(f)} { padding${directionMap[d] || ''} : ${(+s) / 4}rem; }` }],
 ]
