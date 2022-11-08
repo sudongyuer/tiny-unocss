d40650b01f3137ee20729bc0b813696cb469ca0c
Frozen FIsh <976499226@qq.com>
2022-11-08 23:01:20 +0800

    chore: update test

--- packages/tiny-unocss/test/generator.test.ts
+++ packages/tiny-unocss/test/generator.test.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { generator } from '../src/generator'
 import { defaultRules } from '../src/presets'
 
 describe('generator', () => {
-  it('parse tokens', () => {
+  test('parse tokens', () => {
     const code = 'pb-12 text-2xl text-red-500 padding-4;padding-5 "hello"'
     const tokens = new Set(code.split(/[\s'"`;]/g))
     expect(tokens).toMatchInlineSnapshot(`
@@ -19,14 +19,15 @@ describe('generator', () => {
     `)
   })
 
-  it('generator', () => {
+  test('generator', () => {
     const code = 'pl-4 pt-2 pr-8 pb-12'
     const generate = generator(defaultRules)
     const generateCss = generate(code)
     expect(generateCss).toMatchInlineSnapshot(`
       ".pl-4 { padding-left : 1rem; }
       .pt-2 { padding-top : 0.5rem; }
-      .pr-8 { padding-right : 2rem; }"
+      .pr-8 { padding-right : 2rem; }
+      .pb-12 { padding-bottom : 3rem; }"
     `)
   })
 })
